1ae5f33c521a50a34ab7011442249c568d3a5361
Igor Voloshanenko <bob@example.com>
2021-02-06 19:20:53 -0500

    Fix stack name

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -19,7 +19,7 @@ for env_variable in portainer_env_variables:
 portainer_host = os.environ.get('PORTAINER_HOST') or 'http://10.100.101.201:9000/api'
 portainer_username = os.environ.get('PORTAINER_USERNAME') or 'admin'
 portainer_password = os.environ.get('PORTAINER_PASSWORD') or 'Qazwsx!@#123'
-portainer_stack_name = os.environ.get('PORTAINER_STACK_NAME') or 'asteriskcdr'
+portainer_stack_name = os.environ.get('PORTAINER_STACK_NAME') or 'dockerregistry'
 
 def main():
     portainer_config = portainer_api.Configuration()
